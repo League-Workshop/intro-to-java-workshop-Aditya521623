a34f53cffa6d6c4dba6b81696c4edad88750f7b2
League Student <league@iMac.attlocal.net>
2019-08-26 18:25:32 -0700

    we used java on eclipse today
    we created multiple squares and a big pentogans

--- src/section2/FourSquare.java
+++ src/section2/FourSquare.java
@@ -5,28 +5,45 @@ import org.jointheleague.graphical.robot.Robot;
 
 public class FourSquare {
 	
-	// 2. Create a new Robot
+	Robot rob = new Robot();
 
 	void go() {
-		// 4. Make the robot move as fast as possible
+		rob.setSpeed(1000);
 
-		// 5. Set the pen width to 5
+		rob.setPenWidth(5);
+		
+		rob.penDown();
 
-		// 6. Use a for loop to repeat steps #7 to #8, four times...
+		for(int num = 0;  num<4; num++) {
+			
+			rob.setRandomPenColor();
+			
+			drawSquare();
+			
+			rob.turn(90);
+		}
 
-			// 7. Set the pen color to random
+			
 	
-			// 1. Call the drawSquare() method
-	
-			// 8. Turn the robot 90 degrees to the right
+			
 
 	}
 
 	
 	void drawSquare() {
-		JOptionPane.showMessageDialog(null, "yay! you called the drawSquare() method!");
-		/* 3. Fill in the code to draw a square inside the method below. */
+		//JOptionPane.showMessageDialog(null, "yay! you called the drawSquare() method!");
 		
+		
+		
+		
+for(int num = 0;  num<4; num++) {
+			
+			
+			
+			rob.move(200);
+			
+			rob.turn(90);
+		}
 		
 	}
 
--- src/section2/MyFirstJavaProgram.java
+++ src/section2/MyFirstJavaProgram.java
@@ -7,9 +7,26 @@ public class MyFirstJavaProgram {
 	
 	public static void main(String[] args) {
 		
-		// START HERE
-	
+		Robot robo = new Robot();
+		
+		int num = 0;
+		
+		robo.setSpeed(200);
+		
+		robo.penDown();
+		
+		while(num <4) {
+			robo.move(200);
+			
+			robo.turn(90);
+			
+			num++;
+		}
+		 
 		
 		
+		
+		
+		 
 	}
 }
