45206005fce0eeb10bfad72898bb130a392f283f
Aditya521623 <54295036+Aditya521623@users.noreply.github.com>
2019-09-09 18:32:59 -0700

    Add files via upload

--- src/section4/QuizGame.java
+++ src/section4/QuizGame.java
@@ -22,7 +22,47 @@ public class QuizGame {
 		
 		// 3.  Use an if statement to check if their answer is correct
 		
-		// 4.  if the user's answer was correct, add one to their score 
+		if(answer.equals("50") ) {
+			JOptionPane.showMessageDialog(null, "Great Job");
+			
+			userScore =  1;
+		}
+		else
+		{
+			JOptionPane.showMessageDialog(null, "Nice Try");
+			userScore = -1;
+		}
+		
+	
+		
+		String answer2 =JOptionPane.showInputDialog( "What is 15 times 5?");
+		
+		if(answer2.equals("75") ) {
+			JOptionPane.showMessageDialog(null, "Great Job");
+			
+			userScore = userScore + 1 ;
+		}
+		else
+		{
+			JOptionPane.showMessageDialog(null, "Nice Try");
+			userScore = userScore-1;
+		}
+		
+		String answer3 =JOptionPane.showInputDialog( " What is the meaning of life?");
+		
+		if(answer3.equals("Oreos") ) {
+			JOptionPane.showMessageDialog(null, "Great Job, You have found the meaning of your life. ");
+			
+			userScore = userScore + 10000;
+		}
+		else
+		{
+			JOptionPane.showMessageDialog(null, "You have failed to know the meaing of life and you will never be able to taste the greatness of OREOS.");
+			userScore = userScore-1000000000;
+			
+			
+		}
+		JOptionPane.showMessageDialog(null, " Here are yor results: " + userScore );
 		
 		
 		if(userScore<1) {
